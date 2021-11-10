35efa1a52c5fcf0e56d66c928793baa216fcf925
이승훈 <33shlee@naver.com>
2021-11-10 14:12:00 +0900

    code add2

--- chapter06/src/ch05/Wrapper_Ex3.java
+++ chapter06/src/ch05/Wrapper_Ex3.java
@@ -4,12 +4,23 @@ public class Wrapper_Ex3 {
 
 	public static void main(String[] args) {
 		
+		//int --> Integer
+		Integer num = new Integer(17);											// 박싱한 상태
+		int n = num.intValue();													// 언박싱
+		System.out.println(n);
+		
+		
+		Integer num1 = 17;														// 자동 박싱(자동 형변환)
+		int n1 = num1;															// 자동 언박싱(자동 형변환)
+		System.out.println(n);
+		
+		
 		String str = "10";
 		String str2 = "10.5";
 		String str3 = "true";
 		
-		// 8 + "" --> STring으로 자동 변환
-		byte b = Byte.parseByte(str);
+																				// 8 + "" --> String으로 자동 변환
+		byte b = Byte.parseByte(str);											// String --> 기본데이터타입으로 형변환
 		int i = Integer.parseInt(str);
 		short s = Short.parseShort(str);
 		long l = Long.parseLong(str);
@@ -23,11 +34,10 @@ public class Wrapper_Ex3 {
 			System.out.println("boolean형으로 변경 완료");
 		}
 
-		// 문제1
 		String str10 = "1001";
 		String str11 = "2001";
 		
-		// str10 str11을 덧셈 연산 해주세요
+		// str10 str11 덧셈 연산
 		int add = (Integer.parseInt(str10) + Integer.parseInt(str11));
 		System.out.println(add);
 		// 곱셈
@@ -43,7 +53,6 @@ public class Wrapper_Ex3 {
 //		System.out.println(d);
 //		System.out.println(bool);
 		
-		// 한글깨짐 확인코드 !!!!!!
 
 		
 	}
